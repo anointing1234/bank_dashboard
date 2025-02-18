22d2e0b5fa0710335fc7b8cc3eea5535c9e59e7b
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-02-18 13:44:42 +0100

    commited

--- bank_dash/settings.py
+++ bank_dash/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR,'file.env'))
 SECRET_KEY = 'django-insecure-b*fwsqx(&uk0a73ng+_@v2aza0&fe68v8i8kj4%#zw*1y7x@l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://mayb2uc.com"]
