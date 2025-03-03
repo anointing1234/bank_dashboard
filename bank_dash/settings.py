ef409282ce071fbba3af77e93917c6ad3d81799c
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-03-03 09:46:42 +0100

    commited

--- bank_dash/settings.py
+++ bank_dash/settings.py
@@ -37,8 +37,8 @@ SECRET_KEY = 'django-insecure-b*fwsqx(&uk0a73ng+_@v2aza0&fe68v8i8kj4%#zw*1y7x@l4
 DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://mayb2uc.com"]
-ALLOWED_HOSTS = ["mayb2uc.com"]
+CSRF_TRUSTED_ORIGINS = ["https://mayb2cu.com"]
+ALLOWED_HOSTS = ["mayb2cu.com"]
 
 
 
